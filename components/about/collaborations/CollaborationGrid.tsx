1705fe9927eb14f91b203d6b9014a225f57845d7
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2025-01-04 11:29:02 +0530

    image update

--- components/about/collaborations/CollaborationGrid.tsx
+++ components/about/collaborations/CollaborationGrid.tsx
@@ -8,61 +8,61 @@ const COLLABORATIONS = [
     name: "IIT Kanpur",
     type: "Research",
     description: "Kalyanpur, Kanpur, Uttar Pradesh 208016",
-    image: "/patners/1.png"
+    image: "/patners/1.webp"
   },
   {
     name: "IIT Hyderabad",
     type: "Research",
     description: "Kandi, Sangareddy - 502 285' Telangana, India",
-    image: "/patners/2.png"
+    image: "/patners/2.webp"
   },
   {
     name: "IIIT Nagpur",
     type: "Research",
     description: "Nagpur BSNL - Regional Telecom Training Center (RTTC), Near TV Tower or beside Balaji temple Seminary Hills, Nagpur - 440006, Maharashtra",
-    image: "/patners/3.png"
+    image: "/patners/3.webp"
   },
   {
     name: "Amity University",
     type: "Research",
     description: "Amity University Noida Campus Sector-125, Noida - 201313 (U.P.)",
-    image: "/patners/4.png"
+    image: "/patners/4.webp"
   },
   {
     name: "Technorbital Advance Materials Pvt. Ltd.",
     type: "Industry",
     description: "1/1-D, Nawabganj Kanpur - 208 025, Uttar Pradesh,INDIA.",
-    image: "/patners/5.png"
+    image: "/patners/5.webp"
   },
   {
     name: "Bai Labs",
     type: "Lab",
     description: "NL - 115, Northern Laboratory, Manufacturing Science Laboratory Indian Institute of Technology Kanpur U.P. 208016, India.",
-    image: "/patners/6.png"
+    image: "/patners/6.webp"
   },
   {
     name: "Indeema Fibres Pvt. Ltd.",
     type: "Industry",
     description: "A, 1201, SHIVALIK MITTALS ATTALIKA, ARAZI NO 658, SINGHPUR KACHHAR, Kanpur Nagar, Uttar Pradesh, 208017",
-    image: "/patners/7.png"
+    image: "/patners/7.webp"
   },
   {
     name: "Russia, Promenergolab LLC",
     type: "Industry",
     description: "Russia Moscow -105318 Tkackaya Str.1",
-    image: "/patners/8.png"
+    image: "/patners/8.webp"
   },
   {
     name: "NINANO Inc.",
     type: "Industry",
     description: "347 Sanoegangbyeon-Ro, Sanoe-Myeon, Miryang-Si, Gyeongsangnam-Do, 50411",
-    image: "/patners/9.png"
+    image: "/patners/9.webp"
   },
   {
     name: "Egypt, NANOFIBTECH LLC",
     type: "Industry",
     description: "Building No.4 - 151 St El Maadi Cairo",
-    image: "/patners/10.png"
+    image: "/patners/10.webp"
   },
 ] as const;
 
