7da95ec5677c22d69482083c9e0adbc6480425e3
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2025-02-04 16:11:46 +0530

    image cover updates

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -27,7 +27,7 @@ export function ProductCard({ title, description, features, image, index }: Prod
           alt={title}
           fill
           className="object-cover rounded-lg"
-          loading="lazy"
+          // loading="lazy"
         />
       </div>
       
