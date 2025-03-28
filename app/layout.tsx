27db2e746d7a970d7ab66c99bd77729b5db2840a
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2025-03-28 12:10:13 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Header />
         {children}
         <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
